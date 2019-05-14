04d0f47ecc2d016ee784af069335224e8190c5af
Zsolt Nyiscsák <holdfia01@gmail.com>
2019-05-14 23:25:20 +0200

    Beadandó 10-es

--- Beadando1.py
+++ Beadando1.py
@@ -1,3 +1,13 @@
+#Írj egy programot, ami nulla végjelig szavakat kér a standard bemenetből. A szavakat
+#gyűjtse össze egy listába. Ha egy szót másodjára is megkap, akkor kérjen egy másik
+#szót helyette. Majd rendezd a szavakat betűrendbe az első karakterük alapján. Az azonos
+#első karakterrel rendelkező szavak sorrendjéről a szavak hossza (hány karakterből áll)
+#döntsön úgy, hogy először a hosszabb majd a rövidebb szavak következzenek. Ha
+#vannak azonos karakterrel kezdődő és azonos hosszúságú szavak, akkor az álljon
+#előrébb, amit később kapott meg bemenetként. Végül a program írja ki az eredeti és a
+#rendezett listáját is a szavaknak.
+#16
+
 def rendez(lst):
     l = lst.copy()
     for i in range(0,len(lst)-1):       # 2 ciklus egymásban, azért
